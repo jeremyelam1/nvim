7a0614241853bd97e29af85de83b65a363f0d264
lazyspell <jeremy2975@yahoo.com>
2024-08-06 22:36:10 -0700

    minimal go config

--- lua/jelam/plugins/lsp/mason.lua
+++ lua/jelam/plugins/lsp/mason.lua
@@ -2,7 +2,6 @@ return {
 	"williamboman/mason.nvim",
 	dependencies = {
 		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
 	},
 	config = function()
 		-- import mason
@@ -11,7 +10,6 @@ return {
 		-- import mason-lspconfig
 		local mason_lspconfig = require("mason-lspconfig")
 
-		local mason_tool_installer = require("mason-tool-installer")
 
 		-- enable mason and configure icons
 		mason.setup({
@@ -41,17 +39,5 @@ return {
 			},
 		})
 
-		mason_tool_installer.setup({
-			ensure_installed = {
-				"prettier", -- prettier formatter
-				"stylua", -- lua formatter
-				"isort", -- python formatter
-				"black", -- python formatter
-				"pylint", -- python linter
-				"eslint_d", -- js linter
-				"goimports",
-				"gofumpt",
-			},
-		})
 	end,
 }

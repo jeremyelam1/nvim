1d15408a7227d276f42ea76bc9f7d9850ad16bfb
lazyspell <jelam2975@gmail.com>
2024-12-15 01:11:08 -0700

    adding limits onto undofile

--- init.lua
+++ init.lua
@@ -2,3 +2,5 @@ require("jelam.core")
 require("jelam.lazy")
 vim.opt.undofile = true
 vim.opt.undodir = os.getenv("HOME") .. "/.config/nvim/undo"
+vim.opt.undolevels = 1000
+vim.opt.undoreload = 10000

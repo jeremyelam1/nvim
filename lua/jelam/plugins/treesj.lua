0cdc756b855082e14a184941aedd2e46900d6c29
lazyspell <jelam2975@gmail.com>
2024-12-20 00:32:34 -0700

    adding in treesj to plugins

---
+++ lua/jelam/plugins/treesj.lua
@@ -0,0 +1,16 @@
+return {
+	{
+		"Wansmer/treesj",
+		keys = {
+			{ "<leader>mS", "", desc = "TreeSJ" },
+			{ "<leader>mt", "<cmd>TSJToggle<cr>", desc = "Toggle" },
+			{ "<leader>mj", "<cmd>TSJJoin<cr>", desc = "Join" },
+			{ "<leader>ms", "<cmd>TSJSplit<cr>", desc = "Split" },
+		},
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		opts = {
+			use_default_keymaps = false,
+		},
+		config = true,
+	},
+}

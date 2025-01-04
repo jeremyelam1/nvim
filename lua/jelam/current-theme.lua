13c7558aca1122f7ce25b385e0770d3430e70a44
lazyspell <jelam2975@gmail.com>
2025-01-03 20:46:20 -0800

    plugin update

---
+++ lua/jelam/current-theme.lua
@@ -0,0 +1,14 @@
+vim.cmd("colorscheme eldritch")
+
+vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = "none" })
+vim.api.nvim_set_hl(0, "TelescopeResultsNormal", { bg = "none" })
+vim.api.nvim_set_hl(0, "TelescopePreviewNormal", { bg = "none" })
+
+-- Border and Title (optional, for aesthetic consistency)
+vim.api.nvim_set_hl(0, "TelescopePromptBorder", { bg = "none" })
+vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { bg = "none" })
+vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { bg = "none" })
+vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
+vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
+vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NvimTreeNormalNC", { bg = "none" })

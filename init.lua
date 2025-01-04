9902b60a8a077779de9754ab605d2deeb46487e7
lazyspell <jelam2975@gmail.com>
2025-01-03 20:40:29 -0800

    plugin update

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 require("jelam.core")
 require("jelam.lazy")
+require("jelam.current-theme")
 vim.opt.undofile = true
 vim.opt.undodir = os.getenv("HOME") .. "/.config/nvim/undo"
 vim.opt.undolevels = 1000

bab793753d7ce4268bab5dba1d19839dc1aa3c93
lazyspell <jelam2975@gmail.com>
2025-01-08 13:26:13 -0800

    adding and updating plugins

--- init.lua
+++ init.lua
@@ -6,3 +6,13 @@ vim.opt.undofile = true
 vim.opt.undodir = os.getenv("HOME") .. "/.config/nvim/undo"
 vim.opt.undolevels = 1000
 vim.opt.undoreload = 10000
+vim.api.nvim_create_autocmd("CursorMoved", {
+	group = vim.api.nvim_create_augroup("auto-hlsearch", { clear = true }),
+	callback = function()
+		if vim.v.hlsearch == 1 and vim.fn.searchcount().exact_match == 0 then
+			vim.schedule(function()
+				vim.cmd.nohlsearch()
+			end)
+		end
+	end,
+})
